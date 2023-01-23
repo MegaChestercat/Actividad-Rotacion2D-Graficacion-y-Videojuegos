06ac194c60daeb8d6d01f5ab44109d63372b3f66
Mario Arturo Sanchez Ruelas <marioarturosanchez@gmail.com>
2023-01-23 14:36:13 -0600

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Actividad_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PCT_CANVAS = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ResetBTN = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PCT_CANVAS)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1108, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1090, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Generate Square:";
+            // 
+            // PCT_CANVAS
+            // 
+            this.PCT_CANVAS.BackColor = System.Drawing.Color.Black;
+            this.PCT_CANVAS.Location = new System.Drawing.Point(32, 28);
+            this.PCT_CANVAS.Name = "PCT_CANVAS";
+            this.PCT_CANVAS.Size = new System.Drawing.Size(1029, 602);
+            this.PCT_CANVAS.TabIndex = 2;
+            this.PCT_CANVAS.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(1067, 150);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(183, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1067, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Rotation Degrees:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(1067, 262);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(183, 22);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1067, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 64);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Rotation Degrees\n(center of the square in\nthe pivot located at the\ncenter of the " +
+    "screen):";
+            // 
+            // ResetBTN
+            // 
+            this.ResetBTN.Enabled = false;
+            this.ResetBTN.Location = new System.Drawing.Point(1096, 444);
+            this.ResetBTN.Name = "ResetBTN";
+            this.ResetBTN.Size = new System.Drawing.Size(123, 27);
+            this.ResetBTN.TabIndex = 7;
+            this.ResetBTN.Text = "Reset Square";
+            this.ResetBTN.UseVisualStyleBackColor = true;
+            this.ResetBTN.Click += new System.EventHandler(this.ResetBTN_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(1067, 362);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(183, 22);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(1067, 298);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(170, 48);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Rotation Degrees\n(pivot over the center of\nthe default square):";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(1066, 502);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(190, 144);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Instructions:\n1.Generate a new square.\n2.Use one of the textbox\nin order to rotat" +
+    "e in\na particular way.\n3.Reset the square to its\ndefault state so the square\ncan" +
+    " be rotated in a\ndifferent way.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.ResetBTN);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.PCT_CANVAS);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PCT_CANVAS)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox PCT_CANVAS;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button ResetBTN;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Actividad_2
+{
+    public partial class Form1 : Form
+    {
+
+        Bitmap bmp;
+        Graphics g;
+        SolidBrush brush = new SolidBrush(Color.Blue);
+
+        PointF a, b, c, d;
+
+        PointF[] square = new PointF[4];
+        PointF[] rotSquare = new PointF[4];
+        float hHeight, hWidth;
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            bmp = new Bitmap(PCT_CANVAS.Width, PCT_CANVAS.Height);
+            g = Graphics.FromImage(bmp);
+            PCT_CANVAS.Image = bmp;
+
+            hWidth = PCT_CANVAS.Width / 2f;
+            hHeight = PCT_CANVAS.Height / 2f;
+
+            a = new PointF(hWidth - hWidth, hHeight);
+            b = new PointF(hWidth + hWidth, hHeight);
+            c = new PointF(hWidth, hHeight - hHeight);
+            d = new PointF(hWidth, hHeight + hHeight);
+
+            g.DrawLine(Pens.Gray, a, b);
+            g.DrawLine(Pens.Gray, c, d);
+
+            PCT_CANVAS.Refresh();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            textBox3.Enabled = true;
+            DefaultSquare();
+        }
+
+        private void ResetBTN_Click(object sender, EventArgs e)
+        {
+            ResetBTN.Enabled = false;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            textBox3.Enabled = true;
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+
+            hWidth = PCT_CANVAS.Width / 2f;
+            hHeight = PCT_CANVAS.Height / 2f;
+
+            clear();
+            DefaultSquare();
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                textBox1.Enabled = false;
+                textBox3.Enabled = false;
+                clear();
+
+                Double angle = (double.Parse(textBox2.Text) * Math.PI) / 180;
+
+                square[0] = new PointF(hWidth - 25, hHeight + 25);
+                square[1] = new PointF(hWidth + 25, hHeight + 25);
+                square[2] = new PointF(hWidth + 25, hHeight - 25);
+                square[3] = new PointF(hWidth - 25, hHeight - 25);
+
+                Rotate(angle);
+            }
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                clear();
+
+                hWidth = (PCT_CANVAS.Width / 2f) + 25;
+                hHeight = (PCT_CANVAS.Height / 2f) - 25;
+
+                Double angle = (double.Parse(textBox3.Text) * Math.PI) / 180;
+
+                Rotate(angle);
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                textBox3.Enabled = false;
+                textBox2.Enabled = false;
+                clear();
+
+                Double angle = (double.Parse(textBox1.Text)* Math.PI)/180;
+                Rotate(angle);
+            }
+        }
+
+        private void clear()
+        {
+            g.Clear(Color.Transparent);
+            g.DrawLine(Pens.Gray, a, b);
+            g.DrawLine(Pens.Gray, c, d);
+
+            PCT_CANVAS.Refresh();
+        }
+
+        private void Rotate(Double angle)
+        {
+            ResetBTN.Enabled = true;
+
+            rotSquare[0].X = hWidth + (float)(((square[0].X - hWidth) * Math.Cos(angle)) + (square[0].Y - hHeight) * Math.Sin(angle));
+            rotSquare[0].Y = hHeight + (float)((-(square[0].X - hWidth) * Math.Sin(angle)) + (square[0].Y - hHeight) * Math.Cos(angle));
+            rotSquare[1].X = hWidth + (float)(((square[1].X - hWidth) * Math.Cos(angle)) + (square[1].Y - hHeight) * Math.Sin(angle));
+            rotSquare[1].Y = hHeight + (float)((-(square[1].X - hWidth) * Math.Sin(angle)) + (square[1].Y - hHeight) * Math.Cos(angle));
+            rotSquare[2].X = hWidth + (float)(((square[2].X - hWidth) * Math.Cos(angle)) + (square[2].Y - hHeight) * Math.Sin(angle));
+            rotSquare[2].Y = hHeight + (float)((-(square[2].X - hWidth) * Math.Sin(angle)) + (square[2].Y - hHeight) * Math.Cos(angle));
+            rotSquare[3].X = hWidth + (float)(((square[3].X - hWidth) * Math.Cos(angle)) + (square[3].Y - hHeight) * Math.Sin(angle));
+            rotSquare[3].Y = hHeight + (float)((-(square[3].X - hWidth) * Math.Sin(angle)) + (square[3].Y - hHeight) * Math.Cos(angle));
+
+            g.DrawPolygon(Pens.Red, rotSquare);
+            g.FillPolygon(brush, rotSquare);
+
+            PCT_CANVAS.Refresh();
+        }
+
+        private void DefaultSquare()
+        {
+            square[0] = new PointF(hWidth, hHeight);
+            square[1] = new PointF(hWidth + 50, hHeight);
+            square[2] = new PointF(hWidth + 50, hHeight - 50);
+            square[3] = new PointF(hWidth, hHeight - 50);
+
+            g.DrawPolygon(Pens.Red, square);
+            g.FillPolygon(brush, square);
+
+            PCT_CANVAS.Refresh();
+        }
+
+
+    }
+}
